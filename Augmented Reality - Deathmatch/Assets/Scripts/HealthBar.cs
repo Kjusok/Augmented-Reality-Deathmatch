9dff3db5f48eb8ccb7ec7d3589fc454a063011e7
Kjusok <103447181+Kjusok@users.noreply.github.com>
2022-11-20 17:47:44 +0300

    Fixed health bar code
    Fixed health bar code

--- Augmented Reality - Deathmatch/Assets/Scripts/HealthBar.cs
+++ Augmented Reality - Deathmatch/Assets/Scripts/HealthBar.cs
@@ -4,42 +4,98 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    private int _health = 100;
+    [SerializeField] private List<GameObject> _healthDotsList;
+    [SerializeField] private List<float> _valueForDestroyDotsList;
+    [SerializeField] private int _health = 100;
 
-    [SerializeField] private List<Image> _healthDots;
+    private float _healthInOneDot;
+    private float _healthInStart;
 
+
+    private void Awake()
+    {
+        _healthInStart = _health;
+
+        CalculateHealthAtOneDot();
+    }
+
+    private void CalculateHealthAtOneDot()
+    {
+        _healthInOneDot = (float)_health / _healthDotsList.Count;
+
+        var value = 0f;
+
+        for (int i = 0; _valueForDestroyDotsList.Count < _healthDotsList.Count; i++)
+        {
+            _valueForDestroyDotsList.Add(value);
+            value += _healthInOneDot;
+        }
+    }
 
     private void TakeDamage()
     {
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            if (_health != 0)
+            if (_health >= 0)
             {
-                --_health;
-            }
-
-            if (_health % 10 == 0 && _health != 100)
-            {
-                Destroy(_healthDots[_healthDots.Count - 1]);
-                _healthDots.RemoveAt(_healthDots.Count - 1);
+                _health -= 5;
 
                 ChangeColorDots();
+            }
+
+            CheckCurrentHealthDots();
+        }
+    }
+
+    private void CheckCurrentHealthDots()
+    {
+        for (int i = 0; i < _valueForDestroyDotsList.Count; i++)
+        {
+            var dotValue = _valueForDestroyDotsList[i];
+
+            if (_health <= dotValue)
+            {
+                _healthDotsList[i].SetActive(false);
+            }
+            else
+            {
+                _healthDotsList[i].SetActive(true);
             }
         }
     }
 
     private void ChangeColorDots()
     {
-       foreach (Image color in _healthDots)
+        var value = _health  / _healthInStart;
+
+        foreach (GameObject dot in _healthDotsList)
         {
-            color.color = new Color(1,(float) _health / 100, 0);
+            var color = dot.GetComponent<Image>();
+            color.color = new Color(1, value, 0);
         }
     }
 
-   private void Update()
+    private void HealthRecovery()
     {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            _health += 25;
+
+            if (_health > _healthInStart)
+            {
+                _health = (int) _healthInStart;
+            }
+
+            CheckCurrentHealthDots();
+            ChangeColorDots();
+        }
+    }
+
+    private void Update()
+    {
+        HealthRecovery();
         TakeDamage();
-        
+
         if (Camera.main != null)
         {
             var camXform = Camera.main.transform;
